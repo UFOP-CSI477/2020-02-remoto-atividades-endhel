8bd93658ded8e449e1bcea18e250a3af0670e908
endhel <endheltog@gmail.com>
2021-06-23 19:05:48 -0300

    exercicio pratico 01-corrida

--- Atividades/atividade-pratica-01/js/01-corrida.js
+++ Atividades/atividade-pratica-01/js/01-corrida.js
@@ -1,25 +1,56 @@
+function competidor(nome, tempo) {
+
+    this.nome = nome
+    this.tempo = tempo
+
+}
+
 function validar(nome, tempo) {
 
     competidor = nome.value
     tempo_competidor = tempo.value
 
     if (competidor == "" || isNaN(parseInt(tempo_competidor))) {
+
         window.alert('Por favor, preencha os campos corretamente!')
         nome.focus()
         return false
     }
 
-    nome.value = ""
-    tempo.value = ""
-
     return true
 
 }
 
-function carregarDados() {
+function carregarDados(nome, tempo) {
 
-    if (validar(document.dados.nome, document.dados.tempo)) {
-        window.alert('teste')
+    if (validar(nome, tempo)) {
+
+        var qntLinhas = document.getElementById('competidores').rows.length
+
+        if (qntLinhas <= 6) {
+
+            var tb = document.getElementById('competidores')
+            var linha = tb.insertRow(qntLinhas)
+
+            var cellLargada = linha.insertCell(0)
+            var cellNome = linha.insertCell(1)
+            var cellTempo = linha.insertCell(2)
+
+            cellLargada.innerHTML = qntLinhas
+            cellNome.innerHTML = nome.value
+            cellTempo.innerHTML = tempo.value
+
+            nome.value = ""
+            tempo.value = ""
+
+        } else {
+
+            window.alert('O número máximo de competidores foi excedido!')
+
+
+        }
+
+
     }
 
 }
