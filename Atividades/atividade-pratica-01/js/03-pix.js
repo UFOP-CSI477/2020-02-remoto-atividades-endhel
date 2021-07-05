72b79dd1348fafdaac504259077ac93c26d22cf7
endhel <endheltog@gmail.com>
2021-07-05 20:45:39 -0300

    exercício prático 03-pix

--- Atividades/atividade-pratica-01/js/03-pix.js
+++ Atividades/atividade-pratica-01/js/03-pix.js
@@ -1,3 +1,12 @@
+transacoes = []
+
+function Transacao(operacao, valor) {
+
+    this.operacao = operacao
+    this.valor = valor
+
+}
+
 function carregarAgencias() {
 
     fetch("https://brasilapi.com.br/api/banks/v1")
@@ -13,6 +22,7 @@ function inserirAgencias(dados) {
     let agencia = document.getElementById("agencia")
 
     for (i in dados) {
+
         const { name, code } = dados[i]
 
         let opcao = document.createElement('option')
@@ -21,6 +31,7 @@ function inserirAgencias(dados) {
         opcao.innerHTML = `${name}`
 
         agencia.appendChild(opcao)
+
     }
 
 }
@@ -28,9 +39,13 @@ function inserirAgencias(dados) {
 function validar(valor) {
 
     if (valor.value.length > 0) {
+
         return true
+
     } else {
+
         return false
+
     }
 
 }
@@ -47,6 +62,10 @@ function addPix() {
     if (validar(data) && validar(tipo_chave) && validar(chave) && validar(op)
         && validar(agencia) && validar(valor)) {
 
+        const transacao = new Transacao(op.value, valor.value)
+
+        transacoes.push(transacao)
+
         var linha = document.createElement('tr')
         linha.insertCell(0).innerHTML = data.value
         linha.insertCell(1).innerHTML = tipo_chave.value
@@ -56,7 +75,7 @@ function addPix() {
         linha.insertCell(5).innerHTML = parseInt(valor.value)
 
         document.getElementById('tabela').appendChild(linha)
-        document.getElementById('finalizar').disabled = false
+        document.getElementById('fazer_pix').disabled = false
 
         tipo_chave.value = ""
         chave.value = ""
@@ -64,6 +83,59 @@ function addPix() {
         op.value = ""
         valor.value = ""
         data.value = ""
+
+    }
+
+}
+
+function finalizar() {
+
+    let relatorioFinal = document.getElementById("relatorio_final")
+
+    if (relatorio_final.childElementCount > 0) {
+
+        limpar_relatorio(relatorioFinal)
+
+    }
+
+    let qnt_transacoes = transacoes.length
+    let valor_transacoes_recebidas = 0
+    let valor_transacoes_enviadas = 0
+
+    for (let i = 0; i < qnt_transacoes; i++) {
+
+        if (transacoes[i].operacao == "Enviar") {
+
+            valor_transacoes_enviadas += parseFloat(transacoes[i].valor)
+
+        }
+        else {
+
+            valor_transacoes_recebidas += parseFloat(transacoes[i].valor)
+
+        }
+    }
+
+    const saldo_final = valor_transacoes_recebidas - valor_transacoes_enviadas
+
+    let valor_recebidos = document.createElement("p")
+    let valor_enviados = document.createElement("p")
+    let saldoFinal = document.createElement("p")
+
+    valor_recebidos.innerHTML = `Valor total das transações recebidas: R$${valor_transacoes_recebidas.toFixed(2)}`
+    valor_enviados.innerHTML = `Valor total das transações enviadas: R$${valor_transacoes_enviadas.toFixed(2)}`
+    saldoFinal.innerHTML = `Saldo Total : R$${saldo_final.toFixed(2)}`
+
+    relatorioFinal.appendChild(valor_recebidos)
+    relatorioFinal.appendChild(valor_enviados)
+    relatorioFinal.appendChild(saldoFinal)
+}
+
+function limpar_relatorio(relatorio_final) {
+
+    while (relatorio_final.lastElementChild) {
+
+        relatorio_final.removeChild(relatorio_final.lastElementChild)
 
     }
 
